28c94a9341d20908cf3a68d7121b380f33917707
Santhiya <169180434+Santhiya-sm-15@users.noreply.github.com>
2025-04-23 07:34:46 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int tot(int n)
+    {
+        int sum=0;
+        while(n>0)
+        {
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    public int countLargestGroup(int n) {
+        Map<Integer,Integer> m=new HashMap<>();
+        int max=0;
+        int sum=1;
+        for(int j=1;j<=n;j++)
+        {
+            if(j%10==0)
+                sum=tot(j);
+            m.put(sum,m.getOrDefault(sum,0)+1);
+            max=Math.max(max,m.get(sum));
+            sum++;
+        }
+        int cnt=0;
+        for(Map.Entry<Integer,Integer> x:m.entrySet())
+        {
+            if(x.getValue()==max)
+                cnt++;
+        }
+        return cnt;
+    }
+}
